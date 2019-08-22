04c297c503524345b7c37d578f321206019e7124
prashantbhat84 <prashantbhat84@gmail.com>
2019-08-22 14:36:40 +0530

    calculating dimensions dynamically

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  Dimensions,
+  ScrollView
+} from 'react-native';
 import StartGameScreen from './StartGamesScreen';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,28 +16,30 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is over!!!</TitleText>
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is over!!!</TitleText>
 
-      <Image
-        fadeDuration={1000}
-        source={require('../assets/images/success.png')}
-        /*  source={require({
+        <Image
+          fadeDuration={1000}
+          source={require('../assets/images/success.png')}
+          /*  source={require({
           uri: 'https://pixabay.com/go/?t=image-list-shutterstock&id=634991003'
         })} */
-        resizeMode='center'
-        style={styles.images}
-      />
-      <View style={styles.textContainer}>
-        <BodyText style={styles.resultext}>
-          Your Phone required
-          <Text style={styles.highlight}>{props.rounds}</Text> rounds to guess
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
+          resizeMode='center'
+          style={styles.images}
+        />
+        <View style={styles.textContainer}>
+          <BodyText style={styles.resultext}>
+            Your Phone required
+            <Text style={styles.highlight}>{props.rounds}</Text> rounds to guess
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
 
-      <MainButton onPress={props.startNewGame}>NEW GAME</MainButton>
-    </View>
+        <MainButton onPress={props.startNewGame}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -39,9 +49,12 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    borderRadius: 200,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
     borderWidth: 3,
-    borderColor: 'black'
+    borderColor: 'black',
+    marginVertical: Dimensions.get('window').height / 30
   },
   images: {
     width: '80%',
@@ -53,11 +66,11 @@ const styles = StyleSheet.create({
   },
   textContainer: {
     marginHorizontal: 70,
-    marginVertical: 30
+    marginVertical: Dimensions.get('window').height / 60
   },
   resultext: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: Dimensions.get('window').height < 250 ? 16 : 20
   }
 });
 
